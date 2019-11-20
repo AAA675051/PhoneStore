09edd2078664b17fc1275ac5d5a49f148561cbd8
AAA675051 <chamala.pramod@aaa-calif.com>
2019-11-19 16:15:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qneisn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qneisn)
---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: 'Google Pixel XL 4',
+    price: 799,
+    description: 'A large phone with one of the best screens and excellent battery life.'
+  },
+  {
+    name: 'Samsung Galaxy S10',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Moto RAZR',
+    price: 299,
+    description: 'This thing is pretty dope.'
+  },
+
+  {
+    name: 'iPhone 11 Pro Max',
+    price: 1000,
+    description: 'The world\'s best phone! Literally nothing better. '
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
